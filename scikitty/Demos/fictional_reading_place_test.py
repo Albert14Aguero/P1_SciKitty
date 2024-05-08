5a3986b85366005962b6828cf442919d3419bfcf
Alberto <albert14aguero@gmail.com>
2024-05-08 16:30:03 -0600

    Carpeta incorrecta

--- scikitty/Demos/fictional_reading_place_test.py
+++ scikitty/Demos/fictional_reading_place_test.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("../models")
+sys.path.append("../")
 import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
