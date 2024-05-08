0c553f2d96885ba994e1e321f971f0932204c499
ronich06 <48327426+ronich06@users.noreply.github.com>
2024-05-08 16:40:11 -0600

    Final change

--- scikitty/Demos/fictional_reading_place.py
+++ scikitty/Demos/fictional_reading_place.py
@@ -26,7 +26,7 @@ y = df['user_action']              #Tarea 3): Se carga el dataset en la forma us
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)      #Tarea 4): Se crea crean X_train, y_train, X_test, y_test
 
 
-cls = DecisionTree(gini = False)  #Tarea 5): Se entrena con X_train (método fit) el árbol.
+cls = DecisionTree(gini = True)  #Tarea 5): Se entrena con X_train (método fit) el árbol.
 
 cls.fit(X_train, y_train)    #Tarea 5): Se entrena con X_train (método fit) el árbol.    
 
